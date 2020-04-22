67fae99fabe2d4d193e3119541fe85d0dffe464b
Alasdair Wilson <alasdair.wlsn@gmail.com>
2020-04-22 08:45:01 +0100

    renamed file

--- dem_inv_gsvd.py
+++ dem_inv_gsvd.py
@@ -7,26 +7,29 @@ def dem_inv_gsvdcsq(A,B):
 #run gscvd from lapack uisng f2py here (Actually just using numpy now)
     beta=1./np.sqrt(1+s**2)
     alpha=s*beta
-
+    #diagonalise alpha and beta
     onea=np.diag(alpha)
     oneb=np.diag(beta)
 
     w2=inv(inv(onea)@np.transpose(u)@A)
-    #this verification step to check w and w2 recovered from u and v respectively match, turns out the np gsvd returns the transpose of v and not v itself
+    #this verification step to check w and w2 recovered from u and v respectively match, turns out LAPACK 
+    # svd is returns the hermitian transpsoe of v and not v itself
     w=inv(inv(oneb)@v@B)
-    return alpha,beta,u,np.transpose(v),w
+    return alpha,beta,u,np.transpose(v),w,w2
 
 x=np.array([[1,3,5,9],[2,4,-1,2],[1,7,-3,9],[4,-1,1,14]])
 y=np.array([[2,7,1,3],[12,3,-2,-1],[-2,-4,5,1],[-1,7,-1,-3]])
 
 
-a,b,u,v,wt=dem_inv_gsvdcsq(x,y)
+a,b,u,v,wt,w2=dem_inv_gsvdcsq(x,y)
 
 print(a)
 print(b)
 print(u)
 print(v)
 print(wt)
+print(w2)
+
 
 
 
