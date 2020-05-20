4e5bc851fc24f0cece65585060855a69affbdfdf
Alasdair Wilson <alasdair.wlsn@gmail.com>
2020-05-20 13:38:42 +0100

    typo

--- batch_cluster.py
+++ batch_cluster.py
@@ -1,6 +1,6 @@
 from batch_dem_jp2 import batch_dem_jp2
 fits_dir='/home/awilson/code/DEM/demreg-py/demreg-py/test'
-jp2_dir=='/home/awilson/code/DEM/demreg-py/demreg-py/test'
+jp2_dir='/home/awilson/code/DEM/demreg-py/demreg-py/test'
 t_start='2014-01-01 00:00:00.000'
 cadence=1
 nobs=1
