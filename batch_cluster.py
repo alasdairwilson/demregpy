296908a96521c3e0291403e6515482d53ef198ff
Alasdair Wilson <alasdair.wlsn@gmail.com>
2020-10-12 16:24:48 +0100

    cluster changes

--- batch_cluster.py
+++ batch_cluster.py
@@ -1,7 +1,7 @@
 from batch_dem_jp2 import batch_dem_jp2
 fits_dir='/home/awilson/code/DEM/demreg-py/demreg-py/test/'
 jp2_dir='/home/awilson/code/DEM/demreg-py/demreg-py/test/'
-t_start='2014-01-01 00:00:00.000'
-cadence=1
-nobs=1
-batch_dem_jp2(t_start,cadence,nobs,fits_dir,jp2_dir)
+t_start='2013-12-31 12:00:00.000'
+cadence=60*60
+nobs=25
+dem=batch_dem_jp2(t_start,cadence,nobs,fits_dir,jp2_dir,fe_min=5,plot_out=False,plot_loci=False,mk_jp2=True)
