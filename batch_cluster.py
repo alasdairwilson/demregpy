1865cfc4d7c70a797e29344ac9c910567ebd641e
Alasdair Wilson <alasdair.wlsn@gmail.com>
2020-10-12 19:22:49 +0100

    dr change

--- batch_cluster.py
+++ batch_cluster.py
@@ -1,6 +1,6 @@
 from batch_dem_jp2 import batch_dem_jp2
-fits_dir='/scratch/butterfly/demreg-py/fits/'
-jp2_dir='/scratch/butterfly/demreg-py/test/'
+fits_dir='/scratch/butterfly/awilson/demreg-py/fits/'
+jp2_dir='/scratch/butterfly/awilson/demreg-py/test/'
 t_start='2013-12-31 12:00:00.000'
 cadence=60*60
 nobs=25
