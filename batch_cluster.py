9de06c6c9fac43ac9ef42a549ad2280c9ee0f1eb
Alasdair Wilson <alasdair.wlsn@gmail.com>
2020-10-15 16:56:11 +0100

    dir cahnge

--- batch_cluster.py
+++ batch_cluster.py
@@ -1,6 +1,6 @@
 from batch_dem_jp2 import batch_dem_jp2
-fits_dir='/scratch/butterfly/awilson/demreg-py/fits/'
-jp2_dir='/scratch/butterfly/awilson/demreg-py/test/'
+fits_dir='/scratch/mokusei/awilson/demreg-py/fits/'
+jp2_dir='/scratch/mokusei/awilson/demreg-py/test/'
 t_start='2013-12-31 12:00:00.000'
 cadence=60*60
 nobs=25
