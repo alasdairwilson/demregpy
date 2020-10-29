c1e12d994115efb5c60e303ebbd27255edb46b9e
Alasdair Wilson <alasdair.wlsn@gmail.com>
2020-10-29 11:01:34 +0000

    set fits true for cluster

--- batch_cluster.py
+++ batch_cluster.py
@@ -4,4 +4,4 @@ jp2_dir='/scratch/mokusei/awilson/demreg-py/test/'
 t_start='2014-01-01 00:00:00.000'
 cadence=60*60
 nobs=24
-dem=batch_dem_jp2(t_start,cadence,nobs,fits_dir,jp2_dir,fe_min=5,plot_out=False,plot_loci=False,mk_jp2=True)
+dem=batch_dem_jp2(t_start,cadence,nobs,fits_dir,jp2_dir,fe_min=5,plot_out=False,plot_loci=False,mk_jp2=True,mk_fits=True)
