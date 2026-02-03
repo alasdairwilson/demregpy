f0a9d4ecaad9135f6c1068b7e953d4b53df20f4b
Alasdair Wilson <alasdair.wlsn@googlemail.com>
2026-02-03 16:35:36 +0000

    update aiapy imports

--- examples/running_demregpy_on_aia_data.py
+++ examples/running_demregpy_on_aia_data.py
@@ -10,7 +10,8 @@ import math
 import matplotlib.pyplot as plt
 import numpy as np
 import scipy.io as io
-from aiapy.calibrate import correct_degradation, get_correction_table
+from aiapy.calibrate import correct_degradation
+from aiapy.calibrate.utils import get_correction_table
 from astropy import time
 from astropy import units as u
 
